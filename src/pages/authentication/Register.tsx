91b3d66dca318742bcf83c33389126f2258b402b
Dhanushka Gimhan <dhanushkagimhan@gmail.com>
2024-07-06 21:39:27 +0530

    created register form validation

--- src/pages/authentication/Register.tsx
+++ src/pages/authentication/Register.tsx
@@ -1,11 +1,105 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Button, InputBox } from "../../utility/components";
 
+type FormValueType = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  cPassword: string;
+};
+
 export default function Register() {
+  const [formValues, setFormValues] = useState<FormValueType>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cPassword: "",
+  });
+
+  const [formErrors, setFormErrors] = useState<FormValueType>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    cPassword: "",
+  });
+
+  const [submitBtnDisabled, setSubmitBtnDisabled] = useState<boolean>(true);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name: string = e.target.name;
+    const value: string = e.target.value;
+
+    setFormValues((values) => ({ ...values, [name]: value }));
+
+    let submitBtnDisableLocal: boolean = false;
+
+    if (name === "email") {
+      if (
+        value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) == null
+      ) {
+        setFormErrors((values) => ({
+          ...values,
+          [name]: "Please add valid email address",
+        }));
+        submitBtnDisableLocal = true;
+      } else {
+        setFormErrors((values) => ({
+          ...values,
+          [name]: "",
+        }));
+      }
+    }
+
+    if (name === "password") {
+      if (value.length < 8) {
+        setFormErrors((values) => ({
+          ...values,
+          [name]: "Password length must have more than 8 characters",
+        }));
+        submitBtnDisableLocal = true;
+      } else {
+        setFormErrors((values) => ({
+          ...values,
+          [name]: "",
+        }));
+      }
+    }
+
+    if (name === "cPassword") {
+      if (value !== formValues.password) {
+        setFormErrors((values) => ({
+          ...values,
+          [name]: "Confirm password and password are not match!",
+        }));
+        submitBtnDisableLocal = true;
+      } else {
+        setFormErrors((values) => ({
+          ...values,
+          [name]: "",
+        }));
+      }
+    }
+
+    if (
+      formValues.firstName === "" ||
+      formValues.lastName === "" ||
+      formValues.email === "" ||
+      formValues.password === "" ||
+      formValues.cPassword === ""
+    ) {
+      submitBtnDisableLocal = true;
+    }
+
+    setSubmitBtnDisabled(submitBtnDisableLocal);
+  };
+
   const formSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    console.log(e);
+    console.log(formValues);
   };
 
   return (
@@ -14,12 +108,51 @@ export default function Register() {
         <h2 className="text-2xl font-semibold">Register</h2>
 
         <form className="w-[300px]" onSubmit={formSubmit}>
-          {/* <InputBox title="First Name" name="firstName" />
-          <InputBox title="Last Name" name="lastName" />
-          <InputBox title="Email" name="email" />
-          <InputBox title="Password" name="password" />
-          <InputBox title="Confirm Password" name="cPassword" /> */}
-          <Button title="Create Account" type="submit" />
+          <InputBox
+            title="First Name"
+            name="firstName"
+            value={formValues.firstName}
+            handleChange={handleChange}
+            required={true}
+          />
+          <InputBox
+            title="Last Name"
+            name="lastName"
+            value={formValues.lastName}
+            handleChange={handleChange}
+            required={true}
+          />
+          <InputBox
+            title="Email"
+            name="email"
+            type="email"
+            value={formValues.email}
+            handleChange={handleChange}
+            required={true}
+          />
+          <InputBox
+            title="Password"
+            name="password"
+            type="password"
+            value={formValues.password}
+            handleChange={handleChange}
+            required={true}
+            error={formErrors.password}
+          />
+          <InputBox
+            title="Confirm Password"
+            name="cPassword"
+            type="password"
+            value={formValues.cPassword}
+            handleChange={handleChange}
+            required={true}
+            error={formErrors.cPassword}
+          />
+          <Button
+            title="Create Account"
+            type="submit"
+            disable={submitBtnDisabled}
+          />
         </form>
       </div>
     </div>
